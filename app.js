b4151dbe7814ea31c2a089a03acc1677d54bebd1
aliffrhn <alif.farhanw@gmail.com>
2019-01-01 16:52:22 +0700

    change in 3 mins

--- app.js
+++ app.js
@@ -6,5 +6,5 @@ app.listen(port);
 var cron = require('node-cron');
  
 cron.schedule('*/3 * * * *', () => {
-  console.log('running a task every three minutes');
+  console.log('HI! i am cron, i am running a task every three minutes');
 });
