330c87b03b883ed0004af61545a415461500b780
aliffrhn <alif.farhanw@gmail.com>
2019-09-28 15:35:06 +0700

    lst

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 const app = express();
 app.listen(port);
-
+const axios = require('axios');
 var cron = require('node-cron');
  
-cron.schedule('*/3 * * * *', () => {
-  console.log('HI! i am cron, i am running a task every three minutes');
+cron.schedule('*/10 * * * * *', () => {
+  axios.get('http://localhost:3000/v1/sina').then(data => {
+    console.log(true)
+  }).catch(err => {
+    // console.log(err)
+  })
+  console.log('HI! i am cron, i am running a task every 5 detik');
 });
